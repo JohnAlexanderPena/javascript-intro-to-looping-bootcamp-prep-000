641b1ac422b0face8ffe648ad80861d047a9fd25
John Pena <jpena91@gmail.com>
2018-12-27 13:23:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,47 +1,28 @@
-function forLoop(array) {
-  
-  for (let i = 0; i < 25; i++) { 
-  		
-  		if (i === 1 ) {
-        
-         array.push('I am 1 strange loop.');
-   
-    }  else  {  
- 		
- 			array.push(`I am ${i} strange loops.`);
-    }
- }
-return array;
-
+function forLoop(array){
+  for (var i =0; i < 25; i++) {
+   if (i===1) {
+    array.push('I am 1 strange loop.');
+}
+      else array.push(`I am ${i} strange loops.`);
+}
+  return array;
 }
 
-
-function whileLoop(n) {
-  {
-    
-    while (n > 0) 
-    
-     console.log(--n); 
-    
-    }
-     
-       return "done";
-  
+function whileLoop(num) {
+ while (num > 0) {
+   console.log(--num);
+} 
+ return 'done';
 }
 
-function doWhileLoop(array) {
-  
-  var i =0;
- 
+function doWhileLoop(array){ 
+  var i = 0;
 function incrementVariable() {
-  i = i + 1;
+		i = i + 1;
 }
-  
-  do {
-  
-  array.pop(1); }
-    
-    while ((array.length > 0) && incrementVariable());
-  
-  return array; }
-      
+	do {
+		array.pop();
+} while (array.length > 0 && incrementVariable());
+
+return array;
+}
